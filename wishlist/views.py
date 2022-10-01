7ab7cddfd20a10b92c167fe3fe70d68d538eda20
Azra Batrisyia Hisman <azrahisman@gmail.com>
2022-10-01 18:36:05 +0700

    deploy

--- wishlist/views.py
+++ wishlist/views.py
@@ -4,7 +4,7 @@ from wishlist.models import ItemWishlist
 def show_wishlist(request):
     data_wishlist_item = ItemWishlist.objects.all()
     context = {
-    'list_item': data_wishlist_item,
-    'name': 'Azra'
+        'list_item': data_wishlist_item,
+        'name': 'Azra'
     }
     return render(request, "wishlist.html", context)
